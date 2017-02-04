dd89180e84d0b3311be175ab33c78ab4091fe5a3
ZhaoQiling <qilingzhao8@gmail.com>
2017-02-04 10:04:39 +0800

    51_54

--- 1051.cpp
+++ 1051.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+#define PI 3.14159
 using namespace std;
 int main()
 {
-
+    double R1, P1, R2, P2;
+    cin >> R1 >> P1 >> R2 >>P2;
+    double valueA = R1 * R2 * cos(P1 + P2);
+    double valueB = R1 * R2 * sin(P1 + P2);
+    if(valueA > -0.005 && valueA < 0.005)
+        cout << "0.00";
+    else{
+        cout << fixed << setprecision(2) << valueA;
+    }
+    if(valueB > -0.005 && valueB < 0.005)
+    {
+        cout << "+0.00i" << endl;
+    }
+    else if(valueB > 0)
+        cout << "+" << fixed << setprecision(2) << valueB << "i" << endl;
+    else if(valueB < 0)
+        cout << fixed << setprecision(2) << valueB << "i" << endl;
     return 0;
 }
--- 1054.cpp
+++ 1054.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 string input[200];
 double isRight(string& s);
@@ -26,8 +27,10 @@ int main()
     }
     if(nNum == 0)
         cout << "The average of 0 numbers is Undefined";
+    else if(nNum == 1)
+        cout << "The average of " << nNum << " number is " << fixed << setprecision(2) << sum / nNum;
     else
-        cout << "The average of " << nNum << " numbers is " << sum / nNum;
+        cout << "The average of " << nNum << " numbers is " << fixed << setprecision(2) << sum / nNum;
     return 0;
 }
 double isRight(string& s)
@@ -50,5 +53,28 @@ double isRight(string& s)
             return 9999;
         }
     }
-
+    if(s[0] == '.') return 9999;
+    if(s[0] == '-' && s[1] == '.') return 9999;
+    int beforePoint = 0;
+    double afterPoint = 0;
+    int loc = start;
+    for(; loc < s.length(); loc++)
+    {
+        if(s[loc] == '.')
+            break;
+        beforePoint = beforePoint * 10 + (s[loc] - '0');
+    }
+    loc++;
+    for(int i = s.length() - 1; i >= loc; i--)
+    {
+        afterPoint = afterPoint * 0.1 + (s[i] - '0');
+    }
+    afterPoint *= 0.1;
+    if(afterPoint * 100 - int(afterPoint * 100) > 0) return 9999;
+    if(beforePoint > 1000) return 9999;
+    double ans = beforePoint + afterPoint;
+    if(ans == 0 && start == 1) return 9999;
+    if(start == 1) ans *= -1;
+    //cout << ans << endl;
+    return ans;
 }
