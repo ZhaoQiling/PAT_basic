3f1521117da652a6254c3f9406bd27256220c19e
ZhaoQiling <qilingzhao8@gmail.com>
2017-02-07 22:08:49 +0800

    1062

--- 1062.cpp
+++ 1062.cpp
@@ -1,46 +1,40 @@
 #include <iostream>
-#include <stdio.h>
 using namespace std;
-int gcd(int a, int b)
+int gcd(unsigned int a,unsigned int b)
 {
     while(b^=a^=b^=a%=b);
     return a;
 }
 int main()
 {
-    int n1, m1, n2, m2;
-    scanf("%d/%d %d/%d", &n1, &m1, &n2, &m2);
+    int sub1, super1, sub2, super2;
     int k;
+    char u;
+    cin >> sub1 >> u >> super1 >> sub2 >> u >> super2;
     cin >> k;
-    if(double(n1 / m1) > double(n2 / m2))
+    if(double(sub1) / super1 > double(sub2) / super2)
     {
         int temp;
-        temp = n1; n1 = n2; n2 = temp;
-        temp = m1; m1 = m2; m2 = temp;
+        temp = sub1; sub1 = sub2; sub2 = temp;
+        temp = super1; super1 = super1; super1 = super2;
     }
-    int sub1, super1, sub2, super2;
-    int superK;
-    super1 = m1 * m2 * k;
-    super2 = m1 * m2 * k;
-    superK = m1 * m2 * k;
-    sub1 = n1 * m2 * k;
-    sub2 = n2 * m1 * k;
-    int ansSub[1050];
-    int ansSuper[1050];
-    int count = 0;
-    for(int i = sub1 + 1; i < sub2; i++)
+    int ans[1500];
+    int nAns = 0;
+    for(int i = 1; i * super2 < k * sub2; i++)
     {
-        int gcdValue = gcd(i, super1);
-        if(super1 / gcdValue == k && super1 % gcdValue == 0 && i % gcdValue == 0)
+        if(sub1 * k >= super1 * i)
+            continue;
+        //cout << i << endl;
+        if(gcd(i, k) == 1)
         {
-            ansSub[count] = i / gcdValue;
-            count++;
+            ans[nAns] = i;
+            nAns++;
         }
     }
-    for(int i = 0; i < count; i++)
+    for(int i = 0; i < nAns; i++)
     {
-        cout << ansSub[i] << "/" << k;
-        if(i != count - 1)
+        cout << ans[i] << "/" << k;
+        if(i != nAns - 1)
             cout << " ";
     }
     return 0;
